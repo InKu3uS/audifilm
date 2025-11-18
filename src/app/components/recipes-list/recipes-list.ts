399fa0d6bce8a9c8f7d0d8f7e545670facae4bc0
Nefta <neftarguez@gmail.com>
2025-11-18 10:16:05 +0000

    feat: implement recipe filtering and add custom theme support with Angular Material

--- src/app/components/recipes-list/recipes-list.ts
+++ src/app/components/recipes-list/recipes-list.ts
@@ -3,21 +3,52 @@ import { RecipeService } from '../../services/recipes';
 import { Recipe } from '../../models/recipe';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
-import { ButtonModule } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'recipes-list',
   templateUrl: './recipes-list.html',
-  imports: [CommonModule, RouterLink, ButtonModule],
+  imports: [
+    CommonModule,
+    RouterLink,
+    FormsModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatInputModule,
+  ],
 })
 export class RecipesList implements OnInit {
   private readonly service = inject(RecipeService);
 
   recipes: Recipe[] | undefined;
+  filters = {
+    category: '',
+    difficulty: '',
+    duration: 0,
+  };
 
   ngOnInit(): void {
     this.service.getAllRecipes().subscribe((recipes: Recipe[]) => {
       this.recipes = recipes;
     });
+  }
+
+  get filteredRecipes() {
+    return this.recipes
+      ?.filter(
+        (r) =>
+          !this.filters.category ||
+          r.category.toLocaleLowerCase() === this.filters.category.toLocaleLowerCase()
+      )
+      .filter(
+        (r) =>
+          !this.filters.difficulty ||
+          r.difficulty.toLocaleLowerCase() === this.filters.difficulty.toLocaleLowerCase()
+      )
+      .filter((r) => !this.filters.duration || r.duration <= this.filters.duration);
   }
 }
